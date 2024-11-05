dca9840d3f87d51396d65f3b2733cf4717c2d51e
Péter Hanich <hanipet@gmail.com>
2024-11-05 23:12:52 +0100

    Delete unnecessary controllers, add skeleton for WorkController.php

--- writer-reader-backend/app/Http/Controllers/WorkController.php
+++ writer-reader-backend/app/Http/Controllers/WorkController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\StoreWorkRequest;
+use App\Http\Requests\UpdateWorkRequest;
 use App\Http\Resources\WorkResource;
 use App\Models\Work;
 use Illuminate\Http\JsonResponse;
@@ -10,8 +11,59 @@ use Illuminate\Http\Request;
 
 class WorkController extends Controller
 {
- public function postWork(StoreWorkRequest $request): WorkResource
- {
-     return new WorkResource(Work::create($request->all()));
- }
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreWorkRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Work $work)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Work $work)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateWorkRequest $request, Work $work)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Work $work)
+    {
+        //
+    }
 }
