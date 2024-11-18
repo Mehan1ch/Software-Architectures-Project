1dfd6ac995c6626ff2a3e14a830780613197bf60
Verseilus <bucznydominik@gmail.com>
2024-11-18 01:15:01 +0100

    material and navbar

--- writer-reader-web-client/src/App.jsx
+++ writer-reader-web-client/src/App.jsx
@@ -1,12 +1,13 @@
 import { RouterProvider } from "react-router-dom";
 import router from "./router.jsx";
 import { ContextProvider } from "./contexts/ContextProvider.jsx";
-import "./App.css";
+import NavBar from "./components/Navbar.jsx";
 
 function App() {
   return (
     <>
       <ContextProvider>
+        <NavBar />
         <RouterProvider router={router} />
       </ContextProvider>
     </>
