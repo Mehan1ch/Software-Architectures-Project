39927708823c27f3500d93d3495f7378b140ba3f
Verseilus <bucznydominik@gmail.com>
2024-11-22 14:56:20 +0100

    Works page responsive layout

--- writer-reader-web-client/src/pages/Works.jsx
+++ writer-reader-web-client/src/pages/Works.jsx
@@ -64,29 +64,29 @@ export default function Works() {
         <SearchBar />
       </div>
       <Grid2 container spacing={4}>
-        <Grid2 size={3}>
+        <Grid2 size={{ xs: 12, sm: 6, md: 4, lg: 3 }}>
           <WorkCard />
         </Grid2>
-        <Grid2 size={3}>
+        <Grid2 size={{ xs: 12, sm: 6, md: 4, lg: 3 }}>
           <WorkCard />
         </Grid2>
-        <Grid2 size={3}>
+        <Grid2 size={{ xs: 12, sm: 6, md: 4, lg: 3 }}>
           <WorkCard />
         </Grid2>
-        <Grid2 size={3}>
+        <Grid2 size={{ xs: 12, sm: 6, md: 4, lg: 3 }}>
           <WorkCard />
         </Grid2>
 
-        <Grid2 size={3}>
+        <Grid2 size={{ xs: 12, sm: 6, md: 4, lg: 3 }}>
           <WorkCard />
         </Grid2>
-        <Grid2 size={3}>
+        <Grid2 size={{ xs: 12, sm: 6, md: 4, lg: 3 }}>
           <WorkCard />
         </Grid2>
-        <Grid2 size={3}>
+        <Grid2 size={{ xs: 12, sm: 6, md: 4, lg: 3 }}>
           <WorkCard />
         </Grid2>
-        <Grid2 size={3}>
+        <Grid2 size={{ xs: 12, sm: 6, md: 4, lg: 3 }}>
           <WorkCard />
         </Grid2>
       </Grid2>
