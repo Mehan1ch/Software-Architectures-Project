19e76ff91f11a3e68af4fa9fedd0678c9f7619cf
Verseilus <bucznydominik@gmail.com>
2024-11-23 13:58:53 +0100

    User works and collection mock cards

--- writer-reader-web-client/src/components/MyCollections.jsx
+++ writer-reader-web-client/src/components/MyCollections.jsx
@@ -1,3 +1,36 @@
+import { Grid2 } from "@mui/material";
+import CollectionCard from "./CollectionCard";
+
 export default function MyCollections() {
-  return <>Saját gyűjteményeim</>;
+  return (
+    <>
+      <Grid2 container spacing={4}>
+        <Grid2 size={{ xs: 12, sm: 6, md: 4, lg: 3 }}>
+          <CollectionCard />
+        </Grid2>
+        <Grid2 size={{ xs: 12, sm: 6, md: 4, lg: 3 }}>
+          <CollectionCard />
+        </Grid2>
+        <Grid2 size={{ xs: 12, sm: 6, md: 4, lg: 3 }}>
+          <CollectionCard />
+        </Grid2>
+        <Grid2 size={{ xs: 12, sm: 6, md: 4, lg: 3 }}>
+          <CollectionCard />
+        </Grid2>
+
+        <Grid2 size={{ xs: 12, sm: 6, md: 4, lg: 3 }}>
+          <CollectionCard />
+        </Grid2>
+        <Grid2 size={{ xs: 12, sm: 6, md: 4, lg: 3 }}>
+          <CollectionCard />
+        </Grid2>
+        <Grid2 size={{ xs: 12, sm: 6, md: 4, lg: 3 }}>
+          <CollectionCard />
+        </Grid2>
+        <Grid2 size={{ xs: 12, sm: 6, md: 4, lg: 3 }}>
+          <CollectionCard />
+        </Grid2>
+      </Grid2>
+    </>
+  );
 }
--- writer-reader-web-client/src/components/MyWorks.jsx
+++ writer-reader-web-client/src/components/MyWorks.jsx
@@ -1,3 +1,36 @@
+import { Grid2 } from "@mui/material";
+import WorkCard from "./WorkCard";
+
 export default function MyWorks() {
-  return <>Saját műveim</>;
+  return (
+    <>
+      <Grid2 container spacing={4}>
+        <Grid2 size={{ xs: 12, sm: 6, md: 4, lg: 3 }}>
+          <WorkCard />
+        </Grid2>
+        <Grid2 size={{ xs: 12, sm: 6, md: 4, lg: 3 }}>
+          <WorkCard />
+        </Grid2>
+        <Grid2 size={{ xs: 12, sm: 6, md: 4, lg: 3 }}>
+          <WorkCard />
+        </Grid2>
+        <Grid2 size={{ xs: 12, sm: 6, md: 4, lg: 3 }}>
+          <WorkCard />
+        </Grid2>
+
+        <Grid2 size={{ xs: 12, sm: 6, md: 4, lg: 3 }}>
+          <WorkCard />
+        </Grid2>
+        <Grid2 size={{ xs: 12, sm: 6, md: 4, lg: 3 }}>
+          <WorkCard />
+        </Grid2>
+        <Grid2 size={{ xs: 12, sm: 6, md: 4, lg: 3 }}>
+          <WorkCard />
+        </Grid2>
+        <Grid2 size={{ xs: 12, sm: 6, md: 4, lg: 3 }}>
+          <WorkCard />
+        </Grid2>
+      </Grid2>
+    </>
+  );
 }
